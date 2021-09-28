ea96512d380944260cfaf664f7d75f6c33ae3f3b
Rahul Marwaha <Rahul@cloudthing.com>
2021-09-28 12:09:14 +0530

    last minute changes

--- geektrust.js
+++ geektrust.js
@@ -1,9 +1,8 @@
 const checkRuler = require("./src/supportChecker");
 const messageFormSpace = require("./src/file");
-const path = require("path");
 
 const filename = process.argv[2];
 
 filename
-  ? console.log(checkRuler(messageFormSpace(path.join(__dirname, filename))))
+  ? console.log(checkRuler(messageFormSpace(filename)))
   : console.log("NONE");
